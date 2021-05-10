85cd080a7f756839c1d63803b7ce29338a6c3d61
Alistair Mann <al+tfl@pectw.net>
2021-05-10 13:21:17 +0100

    Make UT a tad more elegant

--- test_Maffs.py
+++ test_Maffs.py
@@ -6,30 +6,28 @@ import unittest
 from class_Maffs import Maffs
 
 class TestMaffsMethods(unittest.TestCase):
+    def test_all_nonints_invalid(self):
+        maffs = Maffs
+        self.assertEqual(maffs.average_of_three_non_zeroes(maffs, 'a', 3, 3),
+                        -1)
+        self.assertEqual(maffs.average_of_three_non_zeroes(maffs, 3, 'a', 3),
+                        -1)
+        self.assertEqual(maffs.average_of_three_non_zeroes(maffs, 3, 3, 'a'),
+                        -1)
+
+    def test_all_zeroes_invalid(self):
+        maffs = Maffs
+        self.assertEqual(maffs.average_of_three_non_zeroes(maffs,0, 3, 3),
+                        -1)
+        self.assertEqual(maffs.average_of_three_non_zeroes(maffs, 3, 0, 3),
+                        -1)
+        self.assertEqual(maffs.average_of_three_non_zeroes(maffs, 3, 3, 0),
+                        -1)
+
     def test_most_common(self):
         maffs = Maffs
         self.assertEqual(maffs.average_of_three_non_zeroes(maffs, 3, 3, 3),
                         3)
-
-    def test_v1_notnumber(self):
-        maffs = Maffs
-        self.assertEqual(maffs.average_of_three_non_zeroes(maffs, 'a', 3, 3),
-                        -1)
-
-    def test_v1_zero(self):
-        maffs = Maffs
-        self.assertEqual(maffs.average_of_three_non_zeroes(maffs, 0, 3, 3),
-                        -1)                        
-
-    def test_v2_zero(self):
-        maffs = Maffs
-        self.assertEqual(maffs.average_of_three_non_zeroes(maffs, 3, 0, 3),
-                        -1)
-
-    def test_v3_zero(self):
-        maffs = Maffs
-        self.assertEqual(maffs.average_of_three_non_zeroes(maffs, 3, 3, 0),
-                        -1)
 
     def test_v1v2_zero(self):
         maffs = Maffs
