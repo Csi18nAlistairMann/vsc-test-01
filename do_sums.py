01600e019daa4b7e55f3c7dfbf2f47b8f167b8d1
Alistair Mann <al+tfl@pectw.net>
2021-05-10 10:35:54 +0100

    Change user facing numbers

--- do_sums.py
+++ do_sums.py
@@ -2,9 +2,9 @@ from class_Maffs import Maffs
 
 maffs = Maffs()
 
-num1 = 10
-num2 = 20
-num3 = 30
+num1 = 40
+num2 = 50
+num3 = 60
 av = maffs.average_of_three_non_zeroes(num1, num2, num3)
 if (av == -1):
     msg = "Your numbers are bad and you should feel bad"
